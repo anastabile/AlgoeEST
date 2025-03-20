0ace6f67cbe20fb6126ddef7b1e833850c9d4d7b
Ana Luiza <analuizastabile04@gmail.com>
2025-03-20 14:21:24 -0400

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,6 @@
+n1=int(input("insira o primeiro número"))
+n2=int(input("insira o segundo número"))
+soma=n1+n2
+sub=n1-n2
+mult=n1*n2
+print(f"A soma é {soma}, a subtração  é {sub}, a multiplicação é {mult} ")
---
+++ index.py
@@ -0,0 +1,13 @@
+# comentário
+# print("mensagem") "print" para textos o novo "escreval"
+# idade=6
+# Altura=0.50 #não pode ser virgula apenas ponto
+# peso=5.950
+# nome= "Francisco" #caractere precisa de aspas para rodar
+# print(idade,Altura,peso,nome)
+
+nome=input("insira seu nome") #input serve para o ususario poder inserir texto
+idade=int(input("insira idade")) #int(input para o usuario inserir numero inteiro
+peso=float(input("insira peso")) #float(input para colocar numeros com virgula, quebrados, reais
+print(f"seu nome é {nome}, tem {idade} anos e seu peso é {peso}kg") #f siginifica formatação
+
