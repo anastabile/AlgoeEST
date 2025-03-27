2382d7590a40126cd80cd4a146c50dccb5d3bde3
Ana Luiza <analuizastabile04@gmail.com>
2025-03-27 17:04:58 -0400

    Add files via upload

---
+++ ex8.py
@@ -0,0 +1,10 @@
+login=input("insira o nome de usuário")
+senha=int(input("insira senha"))
+
+if senha==123 and login=="ana":
+    print("login efetuado")
+elif
+ senha!=123 and login!="ana"
+    print("login ou senha incorretos")
+else:
+   print("login ou senha incorretos")
